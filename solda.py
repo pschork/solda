f01920db3248d679dfb0e36f821afdd8c96a129f
Patrick Schork <354473+pschork@users.noreply.github.com>
2024-06-25 00:04:42 +0000

    Change chunk encode to use 31 bytes with 0 prefix for lossless encode Add decode implementation

--- solda.py
+++ solda.py
@@ -15,9 +15,7 @@ RPC_ENDPOINT = os.getenv('RPC_ENDPOINT')
 GRPC_ENDPOINT = os.getenv('GRPC_ENDPOINT')
 ACCOUNT = os.getenv('ACCOUNT')
 
-MAX_SIZE = 2 * 1024 * 1024  # 2 MiB
-CHUNK_SIZE = 32  # 32 bytes
-VALID_RANGE = 21888242871839275222246405745257275088548364400416034343698204186575808495617
+CHUNK_SIZE = 31  # 31 bytes
 
 def chunk_encode(data):
     length_prefix = len(data).to_bytes(4, 'big')
@@ -26,25 +24,21 @@ def chunk_encode(data):
     padded_bytes = length_prefix + data + b'\x00' * padding_length
     chunks = [padded_bytes[i:i + CHUNK_SIZE] for i in range(0, len(padded_bytes), CHUNK_SIZE)]
 
-    # Ensure the chunk values are within VALID_RANGE
-    valid_chunks = []
-    for chunk in chunks:
-        chunk_value = int.from_bytes(chunk, 'big')
-        if chunk_value >= VALID_RANGE:
-            chunk_value %= VALID_RANGE
-            chunk = chunk_value.to_bytes(CHUNK_SIZE, 'big')
-        valid_chunks.append(chunk)
+    # With big endian, set first byte as 0 to ensure data is within valid range of bn254 curve
+    prefixed_chunks = [b'\x00' + chunk for chunk in chunks]
 
-    return b''.join(valid_chunks)
+    return b''.join(prefixed_chunks)
+
 
 def chunk_decode(data):
-    if len(data) % CHUNK_SIZE != 0:
-        raise ValueError("Data length is not a multiple of the chunk size.")
+    if len(data) % (CHUNK_SIZE + 1) != 0:
+        raise ValueError("Data length is not a multiple of the chunk size plus one.")
 
-    combined_bytes = b''.join([data[i:i + CHUNK_SIZE] for i in range(0, len(data), CHUNK_SIZE)])
+    combined_bytes = b''.join([data[i+1:i+1 + CHUNK_SIZE] for i in range(0, len(data), CHUNK_SIZE + 1)])
     length_prefix = combined_bytes[:4]
     json_length = int.from_bytes(length_prefix, 'big')
     return combined_bytes[4:4 + json_length]
+
 
 def disperse_blob(request):
     channel = grpc.secure_channel(GRPC_ENDPOINT, grpc.ssl_channel_credentials())
@@ -61,6 +55,8 @@ snappy_data = chunk_encode(snappy.compress(json_data))
 snappy_size = len(snappy_data)/1024/1024
 
 print(f'Block {slot} {block.blockhash} json:{json_size:.1f}MB snappy:{snappy_size:.1f}MB' )
+
+#assert snappy.decompress(chunk_decode(snappy_data)) == json_data
 
 request = disperser_pb2.DisperseBlobRequest(
     data=snappy_data,
